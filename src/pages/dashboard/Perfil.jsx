92871bcb98455dc14abeba54d3a449e001398629
JJCondoriPinto <juanjosecondoripinto@gmail.com>
2023-12-04 23:49:14 -0500

    plantilla final

--- src/pages/dashboard/Perfil.jsx
+++ src/pages/dashboard/Perfil.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react"
 import ButtonPrimary from "../../components/ButtonPrimary"
 import FormControl from "../../components/FormControl"
+import { EyeIcon, EyeSlashIcon } from "../../components/Icons"
+import useForm from "../../hooks/useForm"
 
 const Perfil = () => {
+
+    const [form, handleInput] = useForm({})
+    const [formPassword, handleInputPassword] = useForm({})
+    const [passwordHidden, setPasswordHidden] = useState(true)
+
+    const [edit, setEdit] = useState(false)
+
     return (
         <>
             <div className="flex gap-4 h-full">
@@ -13,11 +23,17 @@ const Perfil = () => {
                                 name={'nombres'}
                                 type={'text'}
                                 label={'Nombres'}
+                                value={form.nombres || ''}
+                                onInput={handleInput}
+                                readonly={!edit}
                             />
                             <FormControl 
                                 name={'apellidos'}
                                 type={'text'}
                                 label={'Apellidos'}
+                                value={form.apellidos || ''}
+                                onInput={handleInput}
+                                readonly={!edit}
                             />
                         </section>
                         <section className="grid grid-cols-2 gap-4">
@@ -25,11 +41,17 @@ const Perfil = () => {
                                 name={'email'}
                                 type={'email'}
                                 label={'Correo electrónico'}
+                                value={form.email || ''}
+                                onInput={handleInput}
+                                readonly={!edit}
                             />
                             <FormControl 
                                 name={'username'}
                                 type={'text'}
                                 label={'Nombre de usuario'}
+                                value={form.username || ''}
+                                onInput={handleInput}
+                                readonly={!edit}
                             />
                         </section>
                         <section className="grid grid-cols-2 gap-4">
@@ -37,25 +59,66 @@ const Perfil = () => {
                                 name={'documentoIdentificacion'}
                                 type={'number'}
                                 label={'Documento de identificación'}
+                                value={form.documentoIdentificacion || ''}
+                                onInput={handleInput}
+                                readonly={!edit}
                             />
                             <FormControl 
                                 name={'telefono'}
                                 type={'tel'}
                                 label={'Telefono'}
+                                value={form.telefono || ''}
+                                onInput={handleInput}
+                                readonly={!edit}
                             />
                         </section>
                     </div>
                     <div className="flex gap-2 flex-1 justify-end items-end">
-                        <ButtonPrimary className=" ms-auto w-auto">
-                            Actualizar contraseña
-                        </ButtonPrimary>
-                        <ButtonPrimary className="w-auto">
-                            Editar campos
-                        </ButtonPrimary>
+                        {edit ? (
+                            <>
+                                <ButtonPrimary className="w-auto">
+                                    Guardar cambios
+                                </ButtonPrimary>
+                                <ButtonPrimary className="w-auto" onClick={() => setEdit(false)}>
+                                    Cancelar
+                                </ButtonPrimary>
+                            </>
+                        ) : (
+                            <>
+                                <ButtonPrimary className="w-auto" onClick={() => setEdit(true)}>
+                                    Editar campos
+                                </ButtonPrimary>
+                            </>
+                        )}
                     </div>
                 </div>
-                <div className="bg-white p-8 rounded flex-1 text-black">
-                    <header className="text-xl font-semibold mb-10">Mi cuenta de usuario</header>
+                <div className="bg-white p-8 rounded flex-1 text-black flex flex-col">
+                    <header className="text-xl font-semibold mb-10">Mi contraseña</header>
+                    <div className="flex flex-col gap-4">
+                        <FormControl 
+                            type={passwordHidden ? 'password' : 'text'}
+                            name={'password'}
+                            label={'Nueva contraseña'}
+                            value={formPassword.password || ''}
+                            onInput={handleInputPassword}
+                            onInputIcon={() => setPasswordHidden(!passwordHidden)}
+                            icon={!passwordHidden ? <EyeIcon /> : <EyeSlashIcon />}
+                        />
+                        <FormControl 
+                            type={passwordHidden ? 'password' : 'text'}
+                            name={'passwordConfirm'}
+                            label={'Confirme su contraseña nueva'}
+                            value={formPassword.passwordConfirm || ''}
+                            onInput={handleInputPassword}
+                            onInputIcon={() => setPasswordHidden(!passwordHidden)}
+                            icon={!passwordHidden ? <EyeIcon /> : <EyeSlashIcon />}
+                        />
+                    </div>
+                    <div className="flex gap-2 flex-1 justify-end items-end">
+                        <ButtonPrimary className="w-auto" onClick={() => setEditPassword(true)}>
+                            Modificar contraseña
+                        </ButtonPrimary>
+                    </div>
                 </div>
             </div>
         </>
